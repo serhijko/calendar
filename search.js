f895d357905b2ce66dea51b6631d0d7cae7ad896
serhijko <sp_by@tut.by>
2019-05-31 10:42:23 +0300

    add interactivity to event list; styling

--- search.js
+++ search.js
@@ -3,12 +3,15 @@ function onSearchChange() {
   var a = document.getElementById('dropdownList');
   var div = '';
   
-  events.filter(isSearched(searchTerm)).map(item =>
-    div += '<div id="' + item.date + '">' +
+  events.filter(isSearched(searchTerm)).map(item => 
+    div += '<div class="list" id="' + item.date + '" onClick="toEvent(' + (new Date(item.date)).getFullYear() +
+            ', ' + (new Date(item.date)).getMonth() + ')">' +
             '<p class="event">'+ item.event + '</p>' +
-            '<p>' + (new Date(item.date)).getDate() + ' ' +
-            months[(new Date(item.date)).getMonth()] + '</p></div>'
+            '<p class="date">' + (new Date(item.date)).getDate() + ' ' +
+            months[(new Date(item.date)).getMonth()] + '</p></div><hr class="list" />'
   );
+
+  if (div == '') div += '<p>Нет соответствий</p>';
 
   document.querySelector('#dropdownList').innerHTML = div;
 
@@ -25,4 +28,8 @@ function isSearched(searchTerm) {
       months[(new Date(item.date)).getMonth()]).indexOf(searchTerm.toLowerCase()) !== -1 ||
     item.participants.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1;
   }
+}
+
+function toEvent(eventYear, eventMonth) {
+  Calendar(eventYear, eventMonth);
 }
